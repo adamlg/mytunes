a47f12dfadb2d5507f32e99fa13c4db6dd58ee64
MKS-Student <mks.student@hackreactor.com>
2015-03-24 11:41:59 -0500

    added song queue UI

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,32 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   // your code here!
+  initialize: function() {
+    this.on('enqueue dequeue', this.render, this)
+  },
+
+  // render: function() {
+  //   return this.$el;
+  // }
+
+  tagName: 'tr',
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
+
+  events: {
+    'click': function() {
+      this.model.dequeue();
+      // this.model.play();
+    },
+
+    'add remove': function() {
+      this.render();
+    }
+
+  },
+
+  render: function(){
+    console.log(this)
+    return this.$el.html(this);
+  }
 });
